4a0ec82942c48fb5923300a4d14e8fd8adffe2d7
theDomconrad <them.and.us.2013@gmail.com>
2022-10-11 20:16:46 -0700

    sorted through data pulled from recipe fetch, made solid pseudocode and plan with team

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
+$(document).ready(function(){ $('select').formSelect(); }); //to get dropdowns working for food selection
+$("#myChart").hide()
+
 ///////////////////////////////////////////////////////////fetch seperated by long lines like this, below is recipe API(use sparingly, its not free)
 // const options = {
 //     method: 'GET',
 //     headers: {
-//         'X-RapidAPI-Key': '',
+//         'X-RapidAPI-Key': '6e62526b2bmsh6ea8d6b04968f6dp1bf673jsn462c96ed6e67',
 //         'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
 //     }
 // };
@@ -10,7 +13,7 @@
 // userInput = 'pasta'
 // userInput2 = 'italian'
 
-// fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=" +userInput+ "&cuisine=%27+userInput2", options)
+// fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=" +userInput+ "&cuisine="+ userInput2+ "", options)
 //     .then(function (response) {
 //         return response.json();
 //     })
@@ -20,7 +23,7 @@
 //         const set2 = {
 //             method: 'GET',
 //             headers: {
-//                 'X-RapidAPI-Key': '',
+//                 'X-RapidAPI-Key': '6e62526b2bmsh6ea8d6b04968f6dp1bf673jsn462c96ed6e67',
 //                 'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
 //             }
 //         };
@@ -36,6 +39,97 @@
 //     })
 //     .catch(err => console.error(err));
 
+  ///////////////////////////////////PARAMETERS INCLUDING IN SEARCH  WITH CODING GUIDELINES/////////////////////////////////////////
+  //////
+   ////   spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?
+    //  query=pasta ///user text input
+    // &cuisine=italian  ///dropdowns need possible options list stat
+    // &excludeCuisine=greek //same as above line
+    // &diet=vegetarian //row of checkboxes/dropdown
+    // &intolerances=gluten ///row of checkboxes/dropdown
+    // &type=main%20course // dropdown
+    // &instructionsRequired=true //no perameters, include in every search
+    // &addRecipeInformation=true //no perameters, include in every search
+    // &maxCarbs=100 (grams) if userinput (NaN) {write red error message next to input field}
+    // &minProtein=10 (grams) include above 'if' statement for all intiger text returns from user ^^^^^
+    // &maxCalories=800 (grams)
+    // &maxFat=100 (grams)
+    // &maxSaturatedFat=100 (grams)
+    // &minFiber=0 (grams)
+    // &maxSugar=100 (grams)
+    // &maxSodium=100 (milligrams)
+///////////////////////////////////////////HOW TO ACCESS RESULTS, WHICH TO USE//////////////////////////////////////////////////////////////////
+//// title: data.results[i].title
+//// Cook + Prep Time: data.results[i].readyInMinutes
+///  Source URL: data.results[i].sourceUrl  (need to create in shortened hyperlink like make title clickable)
+///  Picture of FOOD: data.results[i].image
+///  Servings: data.results[i].servings
+///  Healthscore: data.results[i].healthScore  (results are /100[outof 100])
+///////////////////////////////////////NUTRITION INFO BELOW (DV = Daily Value %/////////////////////////////////////////////
+//// "Bad":
+////  -Calories : data.bad[0].amount (DV%: data.bad[0].percentOfDailyNeeds [follows same format for every ingredient daily value %])
+////  -Fat:   data.bad[1].amount
+////  -Sat Fat: data.bad[2].amount
+////  -Carbs: data.bad[3].amount
+////  -Cholesterol: data.bad[5].amount
+///   -Sodium: data.bad[6].amount
+///   "Good":
+///   -Protein: data.good[0].amount
+///   -Fiber: data.good[6].amount
+///   -
+
+
+
+
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //         'X-RapidAPI-Key': '6e62526b2bmsh6ea8d6b04968f6dp1bf673jsn462c96ed6e67',
+  //         'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+  //     }
+  // };
+  
+  // userInput = 'pasta'
+  // userInput2 = 'italian'
+  
+  // fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=" +userInput+ "&cuisine="+ userInput2+ "", options)
+  //     .then(function (response) {
+  //         return response.json();
+  //     })
+  //     .then(function (data) {
+  //         console.log(data)
+  //         recipeId = data.results[0].id
+  //         const set2 = {
+  //             method: 'GET',
+  //             headers: {
+  //                 'X-RapidAPI-Key': '6e62526b2bmsh6ea8d6b04968f6dp1bf673jsn462c96ed6e67',
+  //                 'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+  //             }
+  //         };
+  
+  //         fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/' +recipeId+ '/information', set2)
+  //         .then(function (response) {
+  //             return response.json();
+  //         })
+  //         .then(function (data) {
+  //             console.log(data)
+  //         })
+  //         .catch(err => console.error(err));
+  //     })
+  //     .catch(err => console.error(err));
+  /////////////////////////////////////////////////////////////BELOW IS NUTRITION INFORMATION FETCH, NEEDS TO HAPPEN WITH USER FETCH REQUEST//////////////////////////////////////////////////////
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     'X-RapidAPI-Key': '6e62526b2bmsh6ea8d6b04968f6dp1bf673jsn462c96ed6e67',
+  //     'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+  //   }
+  // };
+  
+  // fetch('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/'+recipeID[i]+'/nutritionWidget.json', options)
+  //   .then(response => response.json())
+  //   .then(response => console.log(response))
+  //   .catch(err => console.error(err));
 ////////////////////////////////////////////////////////////////////////////////below is healthy quotes! not free, use sparingly///////////////////////////////////////////////////////////////////////////////
 
 // const options = {
@@ -70,26 +164,34 @@
 
 /////////////////////////////////////chart skeleton just an example:
 
-///////////PSUEDOCODE://///////////////////////  //NEED event listeners corresponding base functions for: //Navbars(Page switching functions) //submit buttons on searches w/ inputs w/ variable inside to be put into api queries //submit buttons also need to append buttons/lists, input into local storage //if appended buttons are called, lets have them pull from local storage instead of API. why??? // here is why: we will be able to re-plug that data back into our content population function // the data is static. it is not the weather, it does not change.    // we need an option for the user to add the meal they select to the calorie counter // the calorie counter will be coded to save the data of each day and display it on page load. //this way the user can keep track of their caloric intake, we also need an option for them to  // put the calories in manually. the chart will function with moment.js intervals and display current dates.  //only make api calls on respective pages(excec
 
-const labels = ["January", "February", "March", "April", "May", "June"];
+  
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
 
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgb(255, 99, 132)",
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
       data: [0, 10, 5, 2, 20, 30, 45],
-    },
-  ],
-};
+    }]
+  };
 
-const config = {
-  type: "line",
-  data: data,
-  options: {},
-};
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
 
-const myChart = new Chart(document.getElementById("myChart"), config);
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
