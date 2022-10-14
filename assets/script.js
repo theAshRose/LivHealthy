8ab8e20bad4d6ea2e76d3fb263c5b67b1903351a
AMESocker <aesocher@yahoo.com>
2022-10-14 09:49:48 -0700

    uncommented the fecth functions, format Homepage and card

--- assets/script.js
+++ assets/script.js
@@ -79,7 +79,7 @@ const optionsQ = {
   },
   body: '{"topic":"health"}',
 };
-/*
+
 fetch("https://pquotes.p.rapidapi.com/api/quote", optionsQ)
   .then(function (response) {
     return response.json();
@@ -97,7 +97,7 @@ fetch("https://pquotes.p.rapidapi.com/api/quote", optionsQ)
 // 	.then(response => response.json())
 // 	.then(response => console.log(response.quote +" by " +response.by)) //response.quote is the quote, response.by is the author
 // 	.catch(err => console.error(err));
-*/
+
 //////////////////////////////////////////////////////////below is excercise///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // const exerciseOptions = {
@@ -194,7 +194,7 @@ const myChart = new Chart(document.getElementById("myChart"), config);
 //
 
 //////////////////////////////////////////////////////////fetch seperated by long lines like this, below is recipe API(use sparingly, its not free)
-/*
+
 function getRecipeAPI(
   cuisine,
   diet,
@@ -275,7 +275,7 @@ function getRecipeAPI(
       console.log(data);
       displayRecipeCards(data);
     });
-}*/
+}
 /////function to dynamically generate recipe cards///
 var foodResultsSection = $("#food-search-results");
 ////project psudocode/////////
