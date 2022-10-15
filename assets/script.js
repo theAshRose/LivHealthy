a6ae152e495bb99bfd917375daeddfbfa2d2ad8c
Chris Gibson <chrischarlesgibson@gmail.com>
2022-10-15 15:32:30 -0500

    hid second recipe title

--- assets/script.js
+++ assets/script.js
@@ -292,6 +292,7 @@ var displayRecipeCards = function (data) {
     cardReveal.addClass("card-reveal");
     cardRevealTitle.addClass("card-title grey-text text-darken-4");
     cardRevealTitle.attr("id", "foodCardTitle2-0", foodRevealTitle);
+    cardRevealTitle.attr("style", "display:none");
     cardRevealIcon.addClass("material-icons right");
     cardRevealList.attr("id", "internalCard0");
     cardRevealBullet.addClass("servingsCard");
